aacb78838c004c1a1a43a435c4ab3b1413c32fc5
AndriiMishch <anderito02@gmail.com>
2023-10-11 20:05:47 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -9,44 +9,41 @@ const amountInp = document.querySelector('input[name="amount"]');
 form.addEventListener('submit', onSubmit);
 
 
-function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const shouldResolve = Math.random() > 0.3;
-      if (shouldResolve) {
-        resolve({ position, delay });
-      } else {
-        reject({ position, delay });
-      }
-    }, delay);
-  });
-}
+const createPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  }, delay);
+});
+
 
 function onSubmit(event) {
   event.preventDefault();
 
-
   let delayValue = Number(delayInp.value);
   const stepValue = Number(stepInp.value);
   const amountValue = Number(amountInp.value);
-if (delayValue <0 || stepValue < 0 || amountValue <= 0) {
-  Notiflix.Report.warning(
-    "Incorrect numbers",
-    'Please choose a correct numbers',
-    'Okay'
-  );
-}  else {
-  for (let i = 1; i <= amountValue; i += 1) {
-    createPromise(i, delayValue)
-      .then(({ position, delay }) => {
-        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
-      });
+  if (delayValue <0 || stepValue < 0 || amountValue <= 0) {
+    Notiflix.Report.warning(
+      "Incorrect numbers",
+      'Please choose a correct numbers',
+      'Okay'
+    );
+  }  else {
+    for (let i = 1; i <= amountValue; i += 1) {
+      createPromise
+        .then(() => {
+          Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(() => {
+          Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+        });
 
-    delayValue += stepValue;
+      delayValue += stepValue;
+    }
   }
-}
-
 }
