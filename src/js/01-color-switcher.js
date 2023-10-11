84d923aa18dc83d1ac90c9608d53d110b07c5135
AndriiMishch <anderito02@gmail.com>
2023-10-11 20:05:15 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,6 @@
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
 const startBtn = document.querySelector("[data-start]");
 const stopBtn = document.querySelector("[data-stop]");
@@ -18,7 +18,7 @@ startBtn.addEventListener("click", () => {
 
 function getBgColor() {
   const color = getRandomHexColor();
-  document.body.style.background = color;
+  body.style.background = color;
 };
 
 
