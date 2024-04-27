ba445ef64cf35d99eb044858f1f2163c7129655b
AndriiMishch <anderito02@gmail.com>
2024-04-27 17:33:58 +0200

    updated timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,16 +1,19 @@
-
+//IMPORTS
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
-
-const daysSpan = document.querySelector('[data-days]');
-const hoursSpan = document.querySelector('[data-hours]');
-const minutesSpan = document.querySelector('[data-minutes]');
-const secondsSpan = document.querySelector('[data-seconds]');
-const startBtn = document.querySelector('button[data-start]');
+// VARIABLES
 const input = document.querySelector('input')
-
+const startBTN = document.querySelector('[data-start]')
+const stopBTN = document.querySelector('[data-stop]')
+const daysValue = document.querySelector('[data-days]')
+const hoursValue = document.querySelector('[data-hours]')
+const minutesValue = document.querySelector('[data-minutes]')
+const secondsValue = document.querySelector('[data-seconds]')
+let date
+let intervalId
+let targetDate
 
 const options = {
   enableTime: true,
@@ -18,69 +21,60 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    targetDate = selectedDates[0];
-    if (targetDate.getTime() < options.defaultDate.getTime()) {
-      Notiflix.Report.warning(
-        "Incorrect date",
-        'Please choose a date in the future',
-        'Okay'
-      );
-      startBtn.toggleAttribute('disabled');
+    currentDate()
+    if(selectedDates[0].getTime() <= date.getTime()){
+      Notiflix.Notify.failure('Please choose a date in the future');
+      startBTN.setAttribute('disabled', true)
     } else {
-        startBtn.removeAttribute('disabled');
+      Notiflix.Notify.success('That is a good date');
+      startBTN.removeAttribute('disabled')
     }
+    targetDate = selectedDates[0].getTime()
   },
 };
 
+//EVENT LISTENERS 
+startBTN.addEventListener('click', ()=>{
+  intervalId = setInterval(()=>{
+    currentDate()
+    let timeLeft = convertMs(targetDate - date.getTime())
+    daysValue.textContent = timeLeft.days
+    hoursValue.textContent = timeLeft.hours
+    minutesValue.textContent = timeLeft.minutes
+    secondsValue.textContent = timeLeft.seconds
+  },1000)
+  input.setAttribute('disabled', true)
+  startBTN.setAttribute('disabled', true)
+  stopBTN.removeAttribute('disabled')
+})
 
-flatpickr('input#datetime-picker', options);
+stopBTN.addEventListener('click', ()=>{
+  clearInterval(intervalId)
+  input.removeAttribute('disabled')
+  startBTN.removeAttribute('disabled')
+  stopBTN.setAttribute('disabled', true)
+})
 
+flatpickr(input, options)
 
-let targetDate = null;
-let timer = null;
-const interval = 1000;
-
-
-startBtn.addEventListener('click', () => {
-  if (targetDate && !timer) { 
-      timer = setInterval(() => {
-          let currentDateInMs = new Date().getTime();
-          let timeDiff = targetDate.getTime() - currentDateInMs;
-          if (timeDiff <= 0) {
-              clearInterval(timer);
-              timer = null; 
-          }
-
-          let remainingTime = convertMs(timeDiff);
-          const timeUnits = {
-              days: daysSpan,
-              hours: hoursSpan,
-              minutes: minutesSpan,
-              seconds: secondsSpan,
-          };
-          
-
-          Object.keys(timeUnits).forEach((unit) => {
-              timeUnits[unit].textContent = String(remainingTime[unit]).padStart(2, '0');
-          });
-
-
-      }, interval);
-      startBtn.toggleAttribute('disabled');
-      input.toggleAttribute('disabled')
-  }
-});
-
+//FUNCTIONS
+function currentDate() {
+  date = new Date
+ }
 
 function convertMs(ms) {
+  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
+  // Remaining days
   const days = Math.floor(ms / day);
+  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
